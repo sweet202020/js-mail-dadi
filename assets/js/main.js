fa08f77ef8650d5632d6f9b347649f0dc52624a1
sweet202020 <dolcealessio1@gmail.com>
2022-10-11 14:20:28 +0200

    initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+alert("it works")
+/* Mail */
+//Chiedi all’utente la sua email
+
+const email = prompt("scrivi la tua e-mail")
+console.log(email)
+//controlla che sia nella lista di chi può accedere
+//stampa un messaggio appropriato sull’esito del controllo.
