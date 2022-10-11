f4fed4ee274086d821d24c3264e2928a972773dd
sweet202020 <dolcealessio1@gmail.com>
2022-10-11 18:00:58 +0200

    final commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,7 +3,7 @@
 //Chiedi all’utente la sua email
 
 const userEmail = prompt("scrivi la tua e-mail");
-console.log(userEmail);
+//console.log(userEmail);
 //controlla che sia nella lista di chi può accedere
 
 //creo un array di e-mail che possono accedere
@@ -14,43 +14,79 @@ const accessEmail = [
     'michaeljordan12@hotmail.it',
     'walterwhite_heisenberg1@gmail.com'
 ]
-console.log(accessEmail); 
-const ulElement = document.querySelector('ul');
+//console.log(accessEmail); 
+
+let isValid = "tu non puoi passare!";
+document.querySelector('p').innerHTML=(isValid)
+/* let notValid; */
+
 
 //utilizzo il for per leggere ogni singola stringa
 for (let i = 0; i < accessEmail.length; i++) {
     const element = accessEmail[i];
-    console.log(element);
+    //console.log(element);
 
     //tramite l'if controllo se l'email dell'utente è tra quelle che può accedere
-    if (userEmail===element){
-        console.log('benvenuto');
-        document.querySelector('p').innerHTML=('benvenuto')
-    } 
+    if (userEmail===element) {
+        isValid = "BENVENUTO!";//'benvenuto';
+        //console.log(isValid);
+    }
 }
 
 
-
+console.log(isValid);
+document.querySelector('p').innerHTML=(isValid)
+//document.querySelector('p').innerHTML=(valid);
 
 //stampa un messaggio appropriato sull’esito del controllo.
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 //Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
 const numPlayer = Math.floor(Math.random()*6+1);
-console.log("il tuo dado è: " + numPlayer);
+const userDado= "il tuo dado è: " + numPlayer;
+document.querySelector('div.user').innerHTML=(userDado);
+
 const numBot = Math.floor(Math.random()*6+1);
-console.log("il dado del bot è: " + numBot);
+const botDado= "il dado del bot è: " + numBot;
+document.querySelector('div.bot').innerHTML=(botDado);
 
 
 // Stabilire il vincitore, in base a chi fa il punteggio più alto.
 
+const ulElement = document.querySelector('ul')
+
 if(numPlayer>numBot){
-    console.log('il vincitore sei tu')
+    const victory = 'il vincitore sei tu'
+    
+    const liElement = `<li>${victory}</li>`;
+    ulElement.insertAdjacentHTML('afterbegin', liElement)
+
 } else if (numPlayer<numBot){
-    console.log("il vincitore è il computer")
+    const lose = "il vincitore è il computer"
+    const liElement = `<li>${lose}</li>`;
+    ulElement.insertAdjacentHTML('afterbegin', liElement)
+
 } else {
-    console.log("è un pareggio")
+    const draw = "è un pareggio"
+    const liElement = `<li>${draw}</li>`;
+    ulElement.insertAdjacentHTML('afterbegin', liElement)
 }
-
-
-
-/* Prima di partire a scrivere codice poniamoci qualche domanda: Che ci sia un array da qualche parte? Se dobbiamo confrontare qualcosa che "cosa" ci serve? */
